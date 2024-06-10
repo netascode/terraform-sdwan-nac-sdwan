77f751f934b3613f1c58c0548bdcdddfaf392036
tzarski0 <92273798+tzarski0@users.noreply.github.com>
2024-06-10 05:11:00 -0700

    System profile and parcels part 1 (#30)

--- sdwan_feature_profiles.tf
+++ sdwan_feature_profiles.tf
@@ -3,3 +3,9 @@ resource "sdwan_cli_feature_profile" "cli_feature_profile" {
   name        = each.value.name
   description = try(each.value.description, "")
 }
+
+resource "sdwan_system_feature_profile" "system_feature_profile" {
+  for_each    = { for t in try(local.feature_profiles.system_profiles, {}) : t.name => t }
+  name        = each.value.name
+  description = try(each.value.description, "")
+}

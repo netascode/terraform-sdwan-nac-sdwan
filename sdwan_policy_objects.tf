e98e3bf907329eb0a8df7de754369645424d754a
Sean Conroy <seconroy@cisco.com>
2024-10-22 16:21:23 +0100

    Add mirror policy object

--- sdwan_policy_objects.tf
+++ sdwan_policy_objects.tf
@@ -22,3 +22,14 @@ resource "sdwan_policy_object_policer" "policy_object_policer" {
     rate_bps          = each.value.rate_bps
   }]
 }
+
+resource "sdwan_policy_object_mirror" "policy_object_mirror" {
+  for_each = { for p in try(local.feature_profiles.policy_object_profile.mirror_lists, {}) : p.name => p }
+  name                = each.value.name
+  description         = try(each.value.description, "")
+  feature_profile_id  = sdwan_policy_object_feature_profile.policy_object_feature_profile[0].id
+  entries = [{
+    remote_destination_ip       = each.value.remote_destination_ip
+    source_ip     = each.value.source_ip
+  }]
+}

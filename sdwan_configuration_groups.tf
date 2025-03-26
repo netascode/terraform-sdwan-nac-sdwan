49f733ae34a9cddc7127e2b7b8f62ef8e89a238b
tzarski0 <tzarski@cisco.com>
2025-03-26 09:42:51 +0100

    update configuration group definition

--- sdwan_configuration_groups.tf
+++ sdwan_configuration_groups.tf
@@ -5,24 +5,12 @@ resource "sdwan_configuration_group" "configuration_group" {
   description = try(each.value.description, "")
   solution    = "sdwan"
   feature_profiles = flatten([
-    try(each.value.cli_profile, null) == null ? [] : [{
-      id = sdwan_cli_feature_profile.cli_feature_profile[each.value.cli_profile].id
-    }],
-    try(each.value.other_profile, null) == null ? [] : [{
-      id = sdwan_other_feature_profile.other_feature_profile[each.value.other_profile].id
-    }],
-    try(each.value.policy_object_profile, null) == null ? [] : [{
-      id = sdwan_policy_object_feature_profile.policy_object_feature_profile[0].id
-    }],
-    try(each.value.service_profile, null) == null ? [] : [{
-      id = sdwan_service_feature_profile.service_feature_profile[each.value.service_profile].id
-    }],
-    try(each.value.system_profile, null) == null ? [] : [{
-      id = sdwan_system_feature_profile.system_feature_profile[each.value.system_profile].id
-    }],
-    try(each.value.transport_profile, null) == null ? [] : [{
-      id = sdwan_transport_feature_profile.transport_feature_profile[each.value.transport_profile].id
-    }]
+    try(sdwan_cli_feature_profile.cli_feature_profile[each.value.cli_profile].id, []),
+    try(sdwan_other_feature_profile.other_feature_profile[each.value.other_profile].id, []),
+    try(sdwan_policy_object_feature_profile.policy_object_feature_profile[each.value.policy_object_profile].id, []),
+    try(sdwan_service_feature_profile.service_feature_profile[each.value.service_profile].id, []),
+    try(sdwan_system_feature_profile.system_feature_profile[each.value.system_profile].id, []),
+    try(sdwan_transport_feature_profile.transport_feature_profile[each.value.transport_profile].id, [])
   ])
   devices = [
     for router in local.routers : {

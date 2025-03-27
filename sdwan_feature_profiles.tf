5de0702165fe76b02ce4a8a314151d117f5cc4b9
tzarski0 <tzarski@cisco.com>
2025-03-27 09:46:28 +0100

    fix policy_object_profile name

--- sdwan_feature_profiles.tf
+++ sdwan_feature_profiles.tf
@@ -30,6 +30,6 @@ resource "sdwan_transport_feature_profile" "transport_feature_profile" {
 
 resource "sdwan_policy_object_feature_profile" "policy_object_feature_profile" {
   count       = contains(keys(local.feature_profiles), "policy_object_profile") ? 1 : 0
-  name        = try(local.feature_profiles.policy_object_profile.name, "Policy_Profile_Global")
+  name        = try(local.feature_profiles.policy_object_profile.name, local.defaults.sdwan.feature_profiles.policy_object_profile.name)
   description = try(local.feature_profiles.policy_object_profile.description, "")
-} 
+}

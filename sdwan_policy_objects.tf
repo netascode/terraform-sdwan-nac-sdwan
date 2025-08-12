bc7dce697f4f173bf53e19fa53078c5131f9ddcd
Pavithran Rathinam <rpaviss@gmail.com>
2025-08-12 17:18:43 +0530

    add support for sdwan_policy_object_app_probe_class (#104)
    * The app probe class module added
    
    * The `description` as null always as not supported by UI. Removed unnecessary nested.
    
    * Update sdwan_configuration_groups.tf
    
    Alphabetical order for newly added object
    
    * Fix formatting
    
    * Fix formatting
    
    * precommit
    
    ---------
    
    Co-authored-by: tzarski0 <92273798+tzarski0@users.noreply.github.com>
    Co-authored-by: tzarski <tzarski@cisco.com>

--- sdwan_policy_objects.tf
+++ sdwan_policy_objects.tf
@@ -1,3 +1,16 @@
+resource "sdwan_policy_object_app_probe_class" "policy_object_app_probe_class" {
+  for_each           = { for p in try(local.feature_profiles.policy_object_profile.app_probe_classes, {}) : p.name => p }
+  name               = each.value.name
+  description        = null # not supported in the UI
+  feature_profile_id = sdwan_policy_object_feature_profile.policy_object_feature_profile[0].id
+  entries = [{
+    forwarding_class = each.value.forwarding_class
+    map = [for m in try(each.value.mappings, []) : {
+      color = m.color
+      dscp  = try(m.dscp, null)
+    }]
+  }]
+}
 resource "sdwan_policy_object_application_list" "policy_object_application_list" {
   for_each           = { for p in try(local.feature_profiles.policy_object_profile.application_lists, {}) : p.name => p }
   name               = each.value.name

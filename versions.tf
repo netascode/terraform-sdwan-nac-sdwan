f3d8c8efd2a4824737e78066a4186cb2ceba9f87
tzarski <tzarski@cisco.com>
2025-10-20 21:49:04 +0200

    update sdwan provider version to >= 0.8.0

--- versions.tf
+++ versions.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     sdwan = {
       source  = "CiscoDevNet/sdwan"
-      version = ">= 0.7.1"
+      version = ">= 0.8.0"
     }
     utils = {
       source  = "netascode/utils"

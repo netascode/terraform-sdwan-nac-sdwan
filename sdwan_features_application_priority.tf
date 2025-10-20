cf7a5da64b6171186c0a1313573ef4500e302cfe
tzarski <tzarski@cisco.com>
2025-10-20 21:49:56 +0200

    fix: correct target interface variable names in application priority QoS policy

--- sdwan_features_application_priority.tf
+++ sdwan_features_application_priority.tf
@@ -10,11 +10,11 @@ resource "sdwan_application_priority_qos_policy" "application_priority_qos_polic
     ])
     : "${qos_item.profile.name}-${qos_item.qos_policy.name}" => qos_item
   }
-  name                      = each.value.qos_policy.name
-  description               = null # not supported in the UI
-  feature_profile_id        = sdwan_application_priority_feature_profile.application_priority_feature_profile[each.value.profile.name].id
-  target_interface          = try(each.value.qos_policy.target_interfaces, null)
-  target_interface_variable = try("{{${each.value.qos_policy.target_interfaces_variable}}}", null)
+  name                       = each.value.qos_policy.name
+  description                = null # not supported in the UI
+  feature_profile_id         = sdwan_application_priority_feature_profile.application_priority_feature_profile[each.value.profile.name].id
+  target_interfaces          = try(each.value.qos_policy.target_interfaces, null)
+  target_interfaces_variable = try("{{${each.value.qos_policy.target_interfaces_variable}}}", null)
   qos_schedulers = try(length(each.value.qos_policy.qos_schedulers) == 0, true) ? null : [for s in each.value.qos_policy.qos_schedulers : {
     bandwidth           = s.bandwidth_percent
     drops               = s.drops

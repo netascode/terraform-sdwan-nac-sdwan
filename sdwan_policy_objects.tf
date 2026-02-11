28222f1249ca91a3284ad26695b5a96b8f2859ba
wei li <wli6@cisco.com>
2026-02-11 18:20:35 +0800

    remove try usage

--- sdwan_policy_objects.tf
+++ sdwan_policy_objects.tf
@@ -4,7 +4,7 @@ resource "sdwan_policy_object_app_probe_class" "policy_object_app_probe_class" {
   description        = null # not supported in the UI
   feature_profile_id = sdwan_policy_object_feature_profile.policy_object_feature_profile[0].id
   entries = [{
-    forwarding_class_id = try(sdwan_policy_object_class_map.policy_object_class_map[each.value.forwarding_class].id, null)
+    forwarding_class_id = sdwan_policy_object_class_map.policy_object_class_map[each.value.forwarding_class].id
     map = [for m in try(each.value.mappings, []) : {
       color = m.color
       dscp  = try(m.dscp, null)

eea37a5e7e4b4f5a8cdd08937cb0b40c968edce2
Alan <alaramir@cisco.com>
2026-03-09 12:57:03 -0600

    adding support for service ospfv3 ipv4 feature

--- sdwan_configuration_groups.tf
+++ sdwan_configuration_groups.tf
@@ -161,6 +161,7 @@ locals {
         try(lan_vpn.multicast, null) == null ? [] : [sdwan_service_lan_vpn_feature_associate_multicast_feature.service_lan_vpn_feature_associate_multicast_feature["${profile.name}-${lan_vpn.name}-routing_multicast"].version],
         try(lan_vpn.ospf, null) == null ? [] : [sdwan_service_lan_vpn_feature_associate_routing_ospf_feature.service_lan_vpn_feature_associate_routing_ospf_feature["${profile.name}-${lan_vpn.name}-routing_ospf"].version],
         try(lan_vpn.ospfv3_ipv6, null) == null ? [] : [sdwan_service_lan_vpn_feature_associate_routing_ospfv3_ipv6_feature.service_lan_vpn_feature_associate_routing_ospfv3_ipv6_feature["${profile.name}-${lan_vpn.name}-routing_ospfv3_ipv6"].version],
+        try(lan_vpn.ospfv3_ipv4, null) == null ? [] : [sdwan_service_lan_vpn_feature_associate_routing_ospfv3_ipv4_feature.service_lan_vpn_feature_associate_routing_ospfv3_ipv4_feature["${profile.name}-${lan_vpn.name}-routing_ospfv3_ipv4"].version],
         sdwan_service_lan_vpn_feature.service_lan_vpn_feature["${profile.name}-${lan_vpn.name}"].version,
         try(lan_vpn.ethernet_interfaces, null) == null ? [] : [for interface in try(lan_vpn.ethernet_interfaces, []) : [
           sdwan_service_lan_vpn_interface_ethernet_feature.service_lan_vpn_interface_ethernet_feature["${profile.name}-${lan_vpn.name}-${interface.name}"].version,
@@ -182,6 +183,9 @@ locals {
       ]],
       try(profile.ospfv3_ipv6_features, null) == null ? [] : [for ospfv3_ipv6_feature in try(profile.ospfv3_ipv6_features, []) : [
         sdwan_service_routing_ospfv3_ipv6_feature.service_routing_ospfv3_ipv6_feature["${profile.name}-${ospfv3_ipv6_feature.name}"].version
+      ]],
+      try(profile.ospfv3_ipv4_features, null) == null ? [] : [for ospfv3_ipv4_feature in try(profile.ospfv3_ipv4_features, []) : [
+        sdwan_service_routing_ospfv3_ipv4_feature.service_routing_ospfv3_ipv4_feature["${profile.name}-${ospfv3_ipv4_feature.name}"].version  
       ]],
       try(profile.route_policies, null) == null ? [] : [for route_policy in try(profile.route_policies, []) : [
         sdwan_service_route_policy_feature.service_route_policy_feature["${profile.name}-${route_policy.name}"].version

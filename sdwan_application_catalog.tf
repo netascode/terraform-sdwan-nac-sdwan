1de4a21c3fe254416bed1c42d6e0dbdd7c1e65bb
wei li <wli6@cisco.com>
2026-08-06 17:10:46 +0800

    alphabetical order

--- sdwan_application_catalog.tf
+++ sdwan_application_catalog.tf
@@ -1,10 +1,8 @@
 resource "sdwan_custom_application" "custom_application" {
   for_each           = { for app in try(local.application_catalog.custom_applications, []) : app.name => app }
   app_name           = each.value.name
-  server_names       = try(each.value.server_names, null)
   application_family = try(each.value.application_family, null)
   application_group  = try(each.value.application_group, null)
-  traffic_class      = try(each.value.traffic_class, null)
   business_relevance = try(each.value.business_relevance, null)
   l3l4 = try(length(each.value.l3l4) > 0, false) ? [
     for item in each.value.l3l4 : {
@@ -13,4 +11,6 @@ resource "sdwan_custom_application" "custom_application" {
       ports        = try(item.ports, null)
     }
   ] : null
+  server_names  = try(each.value.server_names, null)
+  traffic_class = try(each.value.traffic_class, null)
 }
